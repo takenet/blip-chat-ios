48d26db6ba07c4e25a05d5f045b69fd9b2ca2275
Marcelo Amaro <marcelo.amaro@take.net>
2026-07-24 16:32:09 -0300

    chore: remove cocoapods from sample

--- samples/blip-sdk-webview-swift/Pods/Target Support Files/Pods-blip-sdk-webview/Pods-blip-sdk-webview-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_blip_sdk_webview : NSObject
-@end
-@implementation PodsDummy_Pods_blip_sdk_webview
-@end
--- samples/blip-sdk-webview-swift/Pods/Target Support Files/Pods-blip-sdk-webviewTests/Pods-blip-sdk-webviewTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_blip_sdk_webviewTests : NSObject
-@end
-@implementation PodsDummy_Pods_blip_sdk_webviewTests
-@end
